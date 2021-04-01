58d0301a8a5f97f95ea252029e8f8e71de60da6a
Bruno Ignacio Rombolá <49398413+BrunoRombola@users.noreply.github.com>
2021-04-01 15:13:16 -0300

    New elements was added
    I add the class Board with the next methods:
    Board constructor;
    ShowBoardContent(); that shows the board;
    Move(int[] startPos,int[] endPos); that Move to the selected positon;
    MoveOne(int[] startPos,int[] endPos); that moves the selected element 1 position closer to the End position.
    NewTurn(); that makes 4 blank cell to colored cells.

--- Program.cs
+++ Program.cs
@@ -1,61 +1,163 @@
-﻿using System.Linq.Expressions;
-using System.IO.Enumeration;
-using System.IO;
-using System.Security.AccessControl;
+﻿
 using System;
 using System.Collections;
 using System.Collections.Generic;
 
 namespace TOPCODER
 {
-    class Program
+    class Board
     {
-       public static void NewTurn(int[,] grid){
-            var random = new Random();
-            int color, row, column;
-            int i = 4;
-            while( i> 0){
-            color = random.Next(1,5);
-            row = random.Next(grid.GetLength(0));
-            column = random.Next(grid.GetLength(1));
+        private int rows, columns, turn;
+        private int[,] colors;
+        public Board (int rows, int columns){
+            this.rows = rows;
+            this.columns = columns;
+            this.turn = 0;
+            this.colors = new int[rows,columns];
+        }  
+        public void ShowBoardContent(){
+            for(int i = 0; i < colors.GetLength(0); i++){
+                for(int j = 0 ; j < colors.GetLength(1); j++){
+                    Console.Write("{0} ", colors[i,j]);
+                }
+                Console.WriteLine();
+            }
+        }
+        public void Move(int[] startPos,int[] endPos){
+            int[] aux = new int[2];
+            bool wasBreak = false;
+            while(startPos[0] != endPos[0] || startPos[1]!= endPos[1]){
+                if(startPos[0] > endPos[0]){
+                    for(int i = startPos[0]-1; i < startPos[0]+1; i++){
+                        if(startPos[1] > endPos[1]){
+                            for(int j = startPos[1]-1; j < startPos[1]+1; j++){
+                                Console.WriteLine("{0},{1}",i,j);
+                                if(colors[i,j] == 0){
+                                    aux[0] = i;
+                                    aux[1] = j;
+                                    wasBreak = true;
+                                    break;
+                                }
+                            }
+                        }
+                        else{ 
+                           for(int j = startPos[1]+1; j > startPos[1]-1; j--){
+                                Console.WriteLine("{0},{1}",i,j);
+                                if(colors[i,j] == 0){
+                                    aux[0] = i;
+                                    aux[1] = j;
+                                    wasBreak = true;
+                                    break;
+                                }
+                            }
+                        }
+                        if(wasBreak){break;}
+                    }
+                }
+                else{
+                    for(int i = startPos[0]+1; i < startPos[0]-1; i--){
+                        if(startPos[1] > endPos[1]){
+                            for(int j = startPos[1]-1; j < startPos[1]+1; j++){
+                                Console.WriteLine("{0},{1}",i,j);
+                                if(colors[i,j] == 0){
+                                    aux[0] = i;
+                                    aux[1] = j;
+                                    wasBreak = true;
+                                    break;
+                                }
+                            }
+                        }
+                        else{ 
+                           for(int j = startPos[1]+1; j > startPos[1]-1; j--){
+                                Console.WriteLine("{0},{1}",i,j);
+                                if(colors[i,j] == 0){
+                                    aux[0] = i;
+                                    aux[1] = j;
+                                    wasBreak = true;
+                                    break;
+                                }
+                            }
+                        }
+                        if(wasBreak){break;}
+                    }
+                }             
+                if(startPos[0] == endPos[0]){
+                    for(int i = startPos[0]-1; i < startPos[0]+1; i++){
+                        if(startPos[1] > endPos[1]){
+                            for(int j = startPos[1]-1; j < startPos[1]+1; j++){
+                                Console.WriteLine("{0},{1}",i,j);
+                                if(colors[i,j] == 0){
+                                    aux[0] = i;
+                                    aux[1] = j;
+                                    wasBreak = true;
+                                    break;
+                                }
+                            }
+                        }
+                        else{ 
+                           for(int j = startPos[1]+1; j > startPos[1]-1; j--){
+                                Console.WriteLine("{0},{1}",i,j);
+                                if(colors[i,j] == 0){
+                                    aux[0] = i;
+                                    aux[1] = j;
+                                    wasBreak = true;
+                                    break;
+                                }
+                            }
+                        }
+                        if(wasBreak){break;}
+                    }
+                }
+                
+                Console.WriteLine("aux = ({0},{1})",aux[0]+1,aux[1]+1);
+                MoveOne(startPos,aux);
+                startPos[0] = aux[0];
+                startPos[1] = aux[1];
+                wasBreak = false;
+                ShowBoardContent();
+                Console.WriteLine();
+                Console.ReadLine();
+            }  
+        }
+        public void MoveOne(int[] startPos,int[] endPos){
             
-                if(grid[row,column] == 0){
-                    grid[row,column] = color;
-                    i--;
+            colors[endPos[0],endPos[1]] = colors[startPos[0],startPos[1]];
+            colors[startPos[0],startPos[1]] = 0;
+        }
+        public void NewTurn(){
+            Random r = new Random();
+            int posX;
+            int posY;
+            int color, i = 0;
+            while(i<5){
+                color = r.Next(3)+1;
+                posX = r.Next(colors.GetLength(0));
+                posY = r.Next(colors.GetLength(1));
+                if(colors[posX,posY] == 0){
+                    colors[posX,posY] = color;
+                    i++;
                 }
             }
-            
-        }
-        public static void BoardShower(int[,] grid){
-            for(int i = 0; i < grid.GetLength(0); i++){
-                for(int j = 0; j < grid.GetLength(1); j++)
-                    Console.Write(grid[i,j] +" ");
-                Console.Write("\n");     
-            }
-        }
-        public static void MoveElem(int posX, int posY, int newX,int newY, int[,] grid){
-            grid[newX,newY] = grid[posX,posY];
-            grid[posX,posY] = 0;
-        }
-        
+            this.turn++;
 
+        }
+    }
+    class Program
+    {
+        public static int[] SelectElem(){
+            Console.Write("Select the row: ");
+            int posX = Convert.ToInt32(Console.ReadLine())-1;
+            Console.Write("Select the column: ");
+            int posY = Convert.ToInt32(Console.ReadLine())-1;
+            return new int[]{posX,posY};
+        }
         public static void Main(){
-            int[,] grid = new int[6,6];
-            
-            NewTurn(grid);
-            BoardShower(grid);
-            Console.Write("Wich row have your number?\nrow:");
-            int posX = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Wich column have your number?\ncolumn:");
-            int posY = Convert.ToInt32(Console.ReadLine());
-            
-            Console.Write("Wich row have is your number destiny?\n row:");
-            int newX = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Wich column is your number destiny?\ncolumn:");
-            int newY = Convert.ToInt32(Console.ReadLine());
-            MoveElem(posX,posY,newX,newY,grid);
-            BoardShower(grid);    
-            
+            Board board = new Board(6,6);
+            board.NewTurn();
+            board.ShowBoardContent();
+            board.Move(SelectElem(),SelectElem());
+            Console.WriteLine("Final");
+            board.ShowBoardContent();
         }
     }
 }
